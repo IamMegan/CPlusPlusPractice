e9b97c776e089c99fa1e9af9a52789ceaca3ec4a
Megan Johnson <eartmeisterindigo@gmail.com>
2022-12-07 22:39:25 -0500

    Got to a compliable state

--- BookListTest.cpp
+++ BookListTest.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-#include <BookList.cpp>
+#include "BookList.cpp"
 using namespace std;
 
 
-int main(int argc, int argv[]){
+int main(int argc, char *argv[]){
     BookList b;
     string Title, Author, isRead, inProgress, fileName;
     cout << "Enter some books in the order of Title, Author, Whether you have read it or not, and whether you have started it or not: ";
     cin >> Title;
+    cout << endl;
     cin >> Author;
+    cout << endl;
     cin >> isRead;
+    cout << endl;
     cin >> inProgress;
+    cout << endl;
 
     b.constructBook(Title, Author, isRead, inProgress);
     cout << "Enter a file name: ";
